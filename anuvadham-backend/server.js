8e263c9d10f3fbc6d8f8e42b46b83d7ea5e3f946
kusujanakiram <146918430+kusujanakiram@users.noreply.github.com>
2025-05-29 17:30:03 +0530

    Update server.js

--- anuvadham-backend/server.js
+++ anuvadham-backend/server.js
@@ -15,7 +15,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
 app.use(cors({
-  origin: 'http://localhost:5174',
+  origin: 'https://anuvadham.vercel.app',
   credentials: true
 }));
 
